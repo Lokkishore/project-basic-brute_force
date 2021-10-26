de5f3ffb255268519dd5347115f6d5835152ffcf
Lokkishore S <91281112+Lokkishore@users.noreply.github.com>
2021-10-26 10:25:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+import pyautogui
+import  string
+
+
+
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_()][{}|?.,"
+
+# chars = string.printable
+chars_list = list(chars)
+
+
+password = pyautogui.password("Enter a password : ")
+
+guess_password = ""
+
+while(guess_password != password):
+    guess_password = random.choices(chars_list, k=len(password))
+
+    print("<=================="+ str(guess_password)+ "==================>")
+
+    if(guess_password == list(password)):
+        print("Your password is : "+ "".join(guess_password))
+        break
